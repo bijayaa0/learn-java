50611dc87f81878f69952e3513c473bbb20c48ed
bijayaa0 <bijayapun299@gmail.com>
2026-01-02 03:41:37 +0545

    Add HelloWorld example

---
+++ 01_basics/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    public static void main( String[] args ){
+    // this is the main method you need to write to run your code
+
+        System.out.print("Hello, World\n"); //print("hello.\n"); escape sequence for next line.
+        System.out.println("I this is java."); //println(); for next line.
+        System.out.println("nah");
+
+        /* This is
+         for multi-line
+         comment */
+    }
+}
